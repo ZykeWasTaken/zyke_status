c9a848a4ecb6a187701980eb307bfb5f8d7006ec
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2024-11-29 12:17:21 +0100

    feat(base files)

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+author "discord.gg/zykeresources"
+
+shared_scripts {
+    "@zyke_lib/imports.lua",
+    "shared/unlocked/config.lua",
+}
+
+server_scripts {
+    "server/locked/main.lua"
+}
+
+client_scripts {
+    "client/locked/main.lua"
+}
