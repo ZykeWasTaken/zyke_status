9427e26d152263d3ec63dc3b207c751971442f96
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-01-29 23:09:55 +0100

    chore(onSet as required parameter)

--- server/register_statuses.lua
+++ server/register_statuses.lua
@@ -2,7 +2,7 @@
 ---@param name StatusName
 ---@param multi? boolean
 ---@param baseValues table @Values the state will be required to have
----@param functions {onTick: function?, onAdd: function, onRemove: function}
+---@param functions {onTick: function?, onAdd: function, onRemove: function, onSet}
 function RegisterStatusType(name, multi, baseValues, functions)
     Z.debug("Registering", name, multi)
 
