743ccbb51805fd35ef74cfbbf7bb5ffc2625aad2
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-01-29 23:35:08 +0100

    feat(reset status on player respawn)

--- server/eventhandler.lua
+++ server/eventhandler.lua
@@ -1,1 +1,6 @@
 Z.callback.register("zyke_status:GetPlayerStatus", function(plyId) return GetAllRawStatuses(plyId) end)
+
+---@param plyId PlayerId
+RegisterNetEvent("zyke_lib:OnRespawn", function(plyId)
+    ResetStatuses(plyId)
+end)
