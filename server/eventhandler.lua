78f3ae34856aa523207eb774e980b85611bc5d4b
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-01-30 07:45:37 +0100

    feat(prevent reset on first spawn)

--- server/eventhandler.lua
+++ server/eventhandler.lua
@@ -2,5 +2,9 @@ Z.callback.register("zyke_status:GetPlayerStatus", function(plyId) return GetAll
 
 ---@param plyId PlayerId
 RegisterNetEvent("zyke_lib:OnRespawn", function(plyId)
+    -- Only reset if the player is already loaded before
+    -- Otherwise it will cause various issues with selecting your character
+    if (not Cache.statuses[plyId]) then return end
+
     ResetStatuses(plyId)
 end)
