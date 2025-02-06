6c08eafcdc0c43c3ea2d322fa62ca2aea3688b1e
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-02-06 14:23:49 +0100

    chore(removed old effect config)

--- statuses/drunk/config.lua
+++ statuses/drunk/config.lua
@@ -4,7 +4,10 @@ Config.Status.drunk = {
             drain = 0.1
         },
         effect = {
-            {threshold = 15.0, screenEffect = "BikerFilter", walkingStyle = "move_m@drunk@a"}
+            {threshold = 10.0, walkingStyle = "move_m@buzzed"},
+            {threshold = 20.0, screenEffect = "BeastLaunch02", walkingStyle = "move_m@drunk@slightlydrunk"},
+            {threshold = 30.0, screenEffect = "BikerFilter", walkingStyle = "move_m@drunk@a", blurryVision = true},
+            {threshold = 50.0, screenEffect = "DaxTrip03", walkingStyle = "move_m@drunk@verydrunk"},
         },
     }
 }
