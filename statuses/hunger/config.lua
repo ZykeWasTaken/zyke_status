8e0a94a4fd147bfde7d8697fc05f8eaefa5a5dbf
ZykeWasTaken <96230665+ZykeWasTaken@users.noreply.github.com>
2025-02-06 14:44:39 +0100

    chore(removed old effect config)

--- statuses/hunger/config.lua
+++ statuses/hunger/config.lua
@@ -5,14 +5,6 @@ Config.Status.hunger = {
             -- drain = 0.1,
         },
         effect = {
-            -- {
-            --     threshold = 10.0,
-            --     screenEffect = "BeastLaunch02",
-            --     damage = 1, -- Per tick, note that this can not be a decimal due to Gta limitations
-            --     walkingStyle = "move_m@sad@a",
-            --     cameraShaking = 0.5,
-            --     movementSpeed = 0.9,
-            -- }
             {threshold = 30.0, screenEffect = "BeastLaunch02"}, -- Some base screen effect
             {threshold = 20.0, walkingStyle = "move_m@sad@a"}, -- Walking more sluggish
             {threshold = 5.0, screenEffect = "WeaponUpgrade", damage = 1}, -- New screen effect, to simulate the severity, start taking damage, still retaining the walkingStyle from before
