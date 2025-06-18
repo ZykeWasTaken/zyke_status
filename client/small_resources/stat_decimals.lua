c7074e77fff55cc5dd32a4624fb7706eb5e311c3
ZykeWasTaken <linussnilsson03@gmail.com>
2025-06-18 03:43:58 +0200

    fix(handle max)

--- client/small_resources/stat_decimals.lua
+++ client/small_resources/stat_decimals.lua
@@ -13,8 +13,9 @@ local stats = {
 
 ---@param stat "health" | "armor"
 ---@param amount number
-function AddToStat(stat, amount)
-	if (not stats[stat]) then return end
+---@param max? number @The max value you can reach
+function AddToStat(stat, amount, max)
+	if (not stats[stat]) then return end -- Make sure we can even process this stat
 
 	local isPositive = amount > 0.0
 	local newVal = stats[stat] + amount
@@ -26,12 +27,10 @@ function AddToStat(stat, amount)
 		decimal = newVal - math.floor(newVal)
 	end
 
+	stats[stat] = decimal
+
 	-- If the value is between -1.0 and 1.0, we can't process it and just leave it for now
-	if (
-		newVal < 1.0
-		and newVal > -1.0
-	) then
-		stats[stat] = newVal
+	if (newVal < 1.0 and newVal > -1.0) then
 		return
 	end
 
@@ -39,17 +38,40 @@ function AddToStat(stat, amount)
 
 	if (stat == "health") then
 		local currHealth = GetEntityHealth(ply)
+
+		-- If more than max, don't process it
+		if (currHealth >= max) then
+			stats[stat] = 0.0
+			return
+		end
+
 		local isNewValPositive = newVal > 0.0
 		local newHealth = currHealth + (isNewValPositive and math.floor(newVal) or math.ceil(newVal))
 
+		-- If we provide a max, and we can still apply the new value, make sure that we don't exceed the set max
+		if (max) then
+			newHealth = math.min(newHealth, max)
+		end
+
 		SetEntityHealth(ply, newHealth)
-		stats[stat] = decimal
 
 		return
 	elseif (stat == "armor") then
 		local currArmor = GetPedArmour(ply)
+
+		-- If more than max, don't process it
+		if (currArmor >= max) then
+			stats[stat] = 0.0
+			return
+		end
+
 		local isNewValPositive = newVal > 0.0
 		local newArmor = currArmor + (isNewValPositive and math.floor(newVal) or math.ceil(newVal))
+
+		-- If we provide a max, and we can still apply the new value, make sure that we don't exceed the set max
+		if (max) then
+			newArmor = math.min(newArmor, max)
+		end
 
 		SetPedArmour(ply, newArmor)
 		stats[stat] = decimal
