a9288383033cefa12cf4e94ab8e226a0807974de
ZykeWasTaken <linussnilsson03@gmail.com>
2025-06-19 12:48:16 +0200

    fix(mixed events)

--- server/events.lua
+++ server/events.lua
@@ -1,24 +1,24 @@
 Z.callback.register("zyke_status:GetPlayerStatus", function(plyId) return GetAllRawStatuses(plyId) end)
 
 if (Config.Settings.stressEvents.gainStress == true) then
-    RegisterNetEvent("hud:server:RelieveStress", function(amount)
-        if (amount <= 0) then return end
-
-        local prev = GetStatus(source, {"stress"})
-        local newVal = prev - amount
-        if (newVal < 0) then newVal = 0 end
-
-        SetStatusValue(source, {"stress"}, newVal)
-    end)
-end
-
-if (Config.Settings.stressEvents.relieveStress) then
     RegisterNetEvent("hud:server:GainStress", function(amount)
         if (amount < 0) then return end
 
         local prev = GetStatus(source, {"stress"})
         local newVal = prev + amount
         if (newVal > 100) then newVal = 100 end
+
+        SetStatusValue(source, {"stress"}, newVal)
+    end)
+end
+
+if (Config.Settings.stressEvents.relieveStress == true) then
+    RegisterNetEvent("hud:server:RelieveStress", function(amount)
+        if (amount <= 0) then return end
+
+        local prev = GetStatus(source, {"stress"})
+        local newVal = prev - amount
+        if (newVal < 0) then newVal = 0 end
 
         SetStatusValue(source, {"stress"}, newVal)
     end)
