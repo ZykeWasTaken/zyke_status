65111cdb5547b9122cac7185df44f9cbd184b4c5
ZykeWasTaken <linussnilsson03@gmail.com>
2025-09-29 06:10:05 +0200

    feat(simplified adding additional debugs to dev hud)

--- dev/client/hud.lua
+++ dev/client/hud.lua
@@ -1,22 +1,53 @@
 local hudState = GetResourceKvpInt("zyke_status:dev_hud_enabled") or 0
+
+local Drawing = {}
+Drawing.__index = Drawing
+
+function Drawing:new()
+    local self = setmetatable({}, Drawing)
+
+    self.queue = {}
+    self.offset = 0.025
+
+    return self
+end
+
+---@param text string
+function Drawing:addToQueue(text)
+    self.queue[#self.queue+1] = text
+end
+
+function Drawing:draw()
+    for i = 1, #self.queue do
+        Z.drawText(self.queue[i], 0.5, 0.01 + self.offset * (i - 1))
+    end
+end
+
+function Drawing:reset()
+    self.queue = {}
+end
+
 local function devHud()
     CreateThread(function()
+        local drawing = Drawing:new()
+
         while (hudState == 1) do
             local sleep = Cache.statuses and 1 or 3000
 
             if (Cache.statuses) then
-                local offset = 0.0
                 for baseStatus, statusData in pairs(Cache.statuses) do
                     for status, subData in pairs(statusData.values) do
                         for valueKey, value in pairs(subData) do
                             local base = baseStatus ~= status and (baseStatus .. "." .. status) or (status)
 
-                            Z.drawText(base .. "." .. valueKey .. ": " .. value, 0.5, 0.01 + offset)
-                            offset += 0.025
+                            drawing:addToQueue(base .. "." .. valueKey .. ": " .. value)
                         end
                     end
                 end
             end
+
+            drawing:draw()
+            drawing:reset()
 
             Wait(sleep)
         end
